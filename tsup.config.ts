2fb165ca9afd0408ae2aa9ac4692c18e51e48ca3
shindebhau <ajinkya@peerlist.io>
2026-01-16 16:54:01 +0530

    fix: tsup config change and npm ignore

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig({
-  entry: ["src/index.ts", "src/adapters/resend/index.ts"],
-  format: ["cjs", "esm"],
-  dts: true,
-  clean: true,
-  outExtension({ format }) {
-    return { js: format === "esm" ? ".mjs" : ".cjs" };
+export default defineConfig([
+  // CJS build
+  {
+    entry: ["src/index.ts", "src/adapters/resend/index.ts"],
+    format: ["cjs"],
+    dts: true,
+    clean: true,
+    outDir: "dist/cjs",
   },
-});
+  // ESM build
+  {
+    entry: ["src/index.ts", "src/adapters/resend/index.ts"],
+    format: ["esm"],
+    dts: true,
+    outDir: "dist/esm",
+  },
+]);
